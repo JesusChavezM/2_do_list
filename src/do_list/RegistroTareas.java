534e991e5d92241a439737b99db44f886f661bb0
alber <alber@DESKTOP-FMIF03R>
2021-05-06 16:08:32 -0600

    practica 1

---
+++ src/do_list/RegistroTareas.java
@@ -0,0 +1,18 @@
+
+package do_list;
+
+public class RegistroTareas {
+    private String usuario, actividad, actividadNO, secciones;
+    private int fecha1;
+    
+    
+    
+   public RegistroTareas(String actividad, String actividadNO, int fecha1, String secciones){
+     this.actividad=actividad;
+    this.actividadNO=actividadNO;
+    this.fecha1=fecha1;
+    this.secciones=secciones;
+  
+   }
+    
+}
---
+++ src/do_list/To_Do_List.java
@@ -0,0 +1,17 @@
+
+package do_list;
+
+
+public class To_Do_List {
+
+    public static void main(String[] args) {
+        Fecha[] fecha = {new Fecha(17,4,21),
+                         new Fecha(13,4,21),
+                         new Fecha(17,4,22),
+                         new Fecha(20,4,21)}
+                
+        Secciones secciones_ = new Secciones("Lavar los trastes", "ordenar archivos", "Hacer la tarea");   
+        
+    }
+    
+}
