9d8629868da6983a887c76b21f0985a4013db299
Yoselin-Licon <82846173+Yoselin-Licon@users.noreply.github.com>
2021-06-03 18:13:48 -0600

    Version 1.1

---
+++ src/PRUEBA/PRUEBA2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PRUEBA;
+
+/**
+ *
+ * @author Yoselin
+ */
+public class PRUEBA2 {
+   
+    // System.out.println("♥");
+}
